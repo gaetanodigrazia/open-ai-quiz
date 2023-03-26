8bf12cf65284c6a14444d967e4163895e31e428a
gaetanodigrazia <education.digrazia@gmail.com>
2023-03-26 12:21:55 +0200

    Initial commit

--- src/main/java/it/digrazia/kafkaquiz/model/request/QuizResult.java
+++ src/main/java/it/digrazia/kafkaquiz/model/request/QuizResult.java
@@ -1,10 +1,57 @@
 package it.digrazia.kafkaquiz.model.request;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 import java.util.List;
+@Entity
+public class QuizResult {
+    @Id
+    private Long id;
+    private String keycloakUserId;
+    private String username;
+    private Integer result;
+    private String date;
+    public QuizResult()  {
 
-public record QuizResult(Long keycloakUserId, String username, Integer result, String date) {
-    public static QuizResult withDefault(Long keycloakUserId, String username, Integer result, String date)  {
-
-        return new QuizResult(keycloakUserId, username, result, date);
     }
+    public QuizResult(String keycloakUserId, String username, Integer result, String date)  {
+        setKeycloakUserId(keycloakUserId);
+        setUsername(username);
+        setResult(result);
+        setDate(date);
+    }
+    public String getKeycloakUserId() {
+        return keycloakUserId;
+    }
+
+    public void setKeycloakUserId(String keycloakUserId) {
+        this.keycloakUserId = keycloakUserId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getResult() {
+        return result;
+    }
+
+    public void setResult(Integer result) {
+        this.result = result;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+
 }
